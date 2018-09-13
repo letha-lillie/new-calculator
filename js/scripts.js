34b37557c54934f089b2b089a9ca633c0450c76e
Katy and Michael Butcher <katy@>
2018-09-12 20:17:06 -0700

    add radio button asking for feedback

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,8 @@ $(document).ready(function() {
 		var result4 = divide(number7, number8);
 		$(".result4").text(result4);
 
+		$(".feedback").show();
+		var feeling = $("input:radio[name=feeling]:checked").val();
 
 		event.preventDefault();
   });
@@ -37,50 +39,3 @@ $(document).ready(function() {
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// var add = function(number1, number2){
-// 	return number1 + number2;
-// };
-//
-// var number1 = parseInt(prompt("enter a number:"));
-// var number2 = parseInt(prompt("enter another number:"));
-//
-// alert(add(number1, number2));
-//
-// var subtract = function(number1, number2){
-// 	return number1 - number2;
-// };
-//
-// var number1 = parseInt(prompt("enter a number:"));
-// var number2 = parseInt(prompt("enter another number:"));
-//
-// alert(subtract(number1, number2));
-//
-// var multiply = function(number1, number2){
-// 	return number1 * number2;
-// };
-//
-// var number1 = parseInt(prompt("enter a number:"));
-// var number2 = parseInt(prompt("enter another number:"));
-//
-// alert(multiply(number1, number2));
-//
-// var divide = function(number1, number2){
-// 	return number1 / number2;
-// };
-//
-// var number1 = parseInt(prompt("enter a number:"));
-// var number2 = parseInt(prompt("enter another number:"));
-//
-// alert(divide(number1, number2));
