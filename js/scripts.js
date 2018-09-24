961f20f5990a75125a5f719a0393faf9c3045997
Katy Roberts <letha.lillie@gmail.com>
2018-09-23 17:47:58 -0700

    fixed up functions and spruced

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,53 @@
 var add = function(number1, number2){
 	return number1 + number2;
 };
-var subtract = function(number3, number4){
-	return number3 - number4;
+var subtract = function(number1, number2){
+	return number1 - number2;
 };
-var multiply = function(number5, number6){
-	return number5 * number6;
+var multiply = function(number1, number2){
+	return number1 * number2;
 };
-var divide = function(number7, number8){
-	return number7 / number8;
+var divide = function(number1, number2){
+	return number1 / number2;
 };
+
 $(document).ready(function() {
-  $("#calculator form").submit(function(event) {
-    var number1 = parseInt($("input#number1").val());
-    var number2 = parseInt($("input#number2").val());
-    var number3 = parseInt($("input#number3").val());
-    var number4 = parseInt($("input#number4").val());
-    var number5 = parseInt($("input#number5").val());
-    var number6 = parseInt($("input#number6").val());
-		var number7 = parseInt($("input#number7").val());
-    var number8 = parseInt($("input#number8").val());
-		var result1 = add(number1, number2);
-		$(".result1").text(result1);
-		var result2 = subtract(number3, number4);
-		$(".result2").text(result2);
-		var result3 = multiply(number5, number6);
-		$(".result3").text(result3);
-		var result4 = divide(number7, number8);
-		$(".result4").text(result4);
-
-		$(".feedback").show();
-		var feeling = $("input:radio[name=feeling]:checked").val();
-
+  $("form#calculator").submit(function() {
 		event.preventDefault();
-  });
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+		var operator = $("input:radio[name=operator]:checked").val();
+		var result;
+		if (operator === "add"){
+			result = add(number1, number2);
+		} else if (operator === "subtract"){
+			result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
 
-
-
-
+	});
 });
+
+// 		var result1 = add(number1, number2);
+// 		$(".result1").text(result1);
+// 		var result2 = subtract(number3, number4);
+// 		$(".result2").text(result2);
+// 		var result3 = multiply(number5, number6);
+// 		$(".result3").text(result3);
+// 		var result4 = divide(number7, number8);
+// 		$(".result4").text(result4);
+//
+// 		$(".feedback").show();
+// 		var feeling = $("input:radio[name=feeling]:checked").val();
+//
+// 		event.preventDefault();
+//   });
+//
+//
+//
+//
+// });
